faa260085d834d865647bc21a6d6e24b3cada399
Guts-PC <vengattamizhrasigan232@gmail.com>
2026-07-14 17:10:55 +0530

    chore: initialize InspireStack project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+import { Providers } from "@/components/provider";
+
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,10 +28,12 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      lang="en" suppressHydrationWarning
+      className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", inter.variable)}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+             <Providers>{children}</Providers>
+        </body>
     </html>
   );
 }
---
+++ components/provider.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { ThemeProvider } from "next-themes";
+import { Toaster } from "sonner";
+import { ReactNode } from "react";
+
+interface ProvidersProps {
+  children: ReactNode;
+}
+
+export function Providers({ children }: ProvidersProps) {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      {children}
+      <Toaster richColors position="top-right" />
+    </ThemeProvider>
+  );
+}
